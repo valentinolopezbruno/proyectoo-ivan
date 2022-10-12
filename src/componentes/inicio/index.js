ab074cf15b4d8fb852d07614885d1807d924a767
ivanhadrowa <ivanhadrowa0@gmail.com>
2022-10-12 10:37:44 -0300

    fgd

--- src/componentes/inicio/index.js
+++ src/componentes/inicio/index.js
@@ -2,6 +2,7 @@ import "./index.css"
 import "./normalize.css"
 import Logoo from  "../../img/6.png"
 import Iphone from  "../../img/iphone14.png"
+import Airpods from "../../img/airpods.png"
 
 export default function Inicio() {
     return (
@@ -24,7 +25,7 @@ export default function Inicio() {
                 <nav className="menu__desplegable">
                         <div className="navbar">
                                 <a className="enlace-activo" href="#">Inicio</a>
-                                <a href="#comprar">Comprarrrr</a>
+                                <a href="#comprar">Comprar</a>
                                 <a href="">Nosotros</a>
                                 <a href="">Contacto</a>
                         </div>
@@ -64,9 +65,10 @@ export default function Inicio() {
                 </div>
                 
 
-                <a href=""/> <div className="destacado"></div>
+                <a href=""/> <div className="destacado">
                 <img src="/img/watch.png" alt=""/>
                 <h3>Watch</h3>
+                </div>
                 
                 
 
@@ -255,25 +257,12 @@ export default function Inicio() {
                 <div className="scroll">
                 
                 <div className="producto">
-                        <img src="/img/airpods.png" alt=""/>
-                        
-                        <h4>Iphone 13</h4>
-                        <div className="producto-abajo">
-                        <h3>$1.100</h3>
-                        <svg  onclick="add('iphone-13', 1100)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
-                                <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
-                                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                        </svg>
-                        </div>
-                </div>
-
-                <div className="producto">
-                        <img src="/img/airpods.png" alt=""/>
+                        <img src={Airpods} alt=""/>
                         
                         <h4>Iphone 13</h4>
                         <div className="producto-abajo">
                                 <h3>$1.100</h3>
-                                <svg  onclick="add('iphone-13', 1100)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
+                                <svg  onclick="add('iphone-13', 250)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
                                 <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
                                 <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                                 </svg>
@@ -281,12 +270,25 @@ export default function Inicio() {
                 </div>
 
                 <div className="producto">
-                        <img src="/img/airpods.png" alt=""/>
+                        <img src={Airpods} alt=""/>
                         
                         <h4>Iphone 13</h4>
                         <div className="producto-abajo">
                                 <h3>$1.100</h3>
-                                <svg  onclick="add('iphone-13', 1100)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
+                                <svg  onclick="add('iphone-13', 250)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
+                                <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+                                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                </svg>
+                        </div>
+                </div>
+
+                <div className="producto">
+                        <img src={Airpods} alt=""/>
+                        
+                        <h4>Iphone 13</h4>
+                        <div className="producto-abajo">
+                                <h3>$1.100</h3>
+                                <svg  onclick="add('iphone-13', 250)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
                                 <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
                                 <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                                 </svg>
@@ -295,25 +297,12 @@ export default function Inicio() {
 
 
                 <div className="producto">
-                        <img src="/img/airpods.png" alt=""/>
+                        <img src={Airpods} alt=""/>
                         
                         <h4>Iphone 13</h4>
                         <div className="producto-abajo">
                                 <h3>$1.100</h3>
-                                <svg  onclick="add('iphone-13', 1100)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
-                                <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
-                                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                                </svg>
-                                </div>
-                </div>
-
-                <div className="producto">
-                        <img src="/img/airpods.png" alt=""/>
-                        
-                        <h4>Iphone 13</h4>
-                        <div className="producto-abajo">
-                                <h3>$1.100</h3>
-                                <svg  onclick="add('iphone-13', 1100)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
+                                <svg  onclick="add('iphone-13', 250)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
                                 <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
                                 <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                                 </svg>
@@ -321,12 +310,25 @@ export default function Inicio() {
                 </div>
 
                 <div className="producto">
-                        <img src="../img/airpods.png" alt=""/>
+                        <img src={Airpods} alt=""/>
                         
                         <h4>Iphone 13</h4>
                         <div className="producto-abajo">
                                 <h3>$1.100</h3>
-                                <svg  onclick="add('iphone-13', 1100)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
+                                <svg  onclick="add('iphone-13', 250)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
+                                <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+                                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                </svg>
+                        </div>
+                </div>
+
+                <div className="producto">
+                        <img src={Airpods} alt=""/>
+                        
+                        <h4>Iphone 13</h4>
+                        <div className="producto-abajo">
+                                <h3>$1.100</h3>
+                                <svg  onclick="add('iphone-13', 250)" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-cart-plus carrito" viewBox="0 0 16 16">
                                 <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
                                 <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                                 </svg>
